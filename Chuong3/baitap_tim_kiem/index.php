b35534c4745019e22a1b8a5d61bf540b62aa0174
STUDENT <tranvanhoa15042000@gmail.com>
2020-08-22 19:20:02 +0700

    bai tap

--- Chuong3/baitap_tim_kiem/index.php
+++ Chuong3/baitap_tim_kiem/index.php
@@ -7,7 +7,7 @@
     <title>Tìm Kiếm</title>
 </head>
 <body>
-    <form action="xuly.php" method="GET" name="frmTiemKiem" id="frmTiemKiem">
+    <form action="" method="GET" name="frmTiemKiem" id="frmTiemKiem">
         Tên sản phẩm <br/>
         <input type="text" name="txtTenSP" id="txtTenSP"><br/>
         Loại sản phẩm <br/>
@@ -23,7 +23,45 @@
         Khuyến mãi<br/>
         <input type="radio" name="khuyenMai" id="khuyenMai_1" value="1"> Trung Thu<br/>
         <input type="radio" name="khuyenMai" id="khuyenMai_2" value="2"> Sinh Nhật<br/>
-        <input type="submit" value="Tìm Kiếm">
+        <input type="submit" value="Tìm Kiếm" name="y">
+        
     </form>
+    <?php
+        if(isset($_GET['y'])){
+            $txtTenSP=$_GET['txtTenSP'];
+            $loaiSP = [];
+            if(isset($_GET['loaiSP'])){
+                $loaiSP = $_GET['loaiSP'];
+            }
+            $nhaSX = [];
+            if(isset($_GET['nhaSX'])){
+                $nhaSX = $_GET['nhaSX'];
+            }
+            $khuyenMai = null;
+            if(isset($_GET['khuyenMai'])){
+                $khuyenMai = $_GET['khuyenMai'];
+            }
+
+
+            echo "<ul>";
+            echo "<li>Tên sản phẩm : {$txtTenSP}</li>";
+            if(!empty($loaiSP)){
+                $listLoaiSP = implode(',',$loaiSP);
+                echo "<li>Loại sản phẩm : {$listLoaiSP}</li>";
+            }
+            if(!empty($nhaSX)){
+                $listNhaSX = implode(',',$nhaSX);
+                echo "<li>Nhà sản xuất : {$listNhaSX}</li>";
+            }
+            if(!empty($khuyenMai)){
+                if($khuyenMai=="1"){
+                echo "<li>Khuyến Mãi : Trung Thu</li>";
+                }else{
+                    echo "<li>Khuyến Mãi : Sinh Nhật</li>";
+                }
+            }
+            echo "</ul>";
+        }
+    ?>
 </body>
 </html>
