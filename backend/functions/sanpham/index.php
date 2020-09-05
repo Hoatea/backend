fe2fdc141cb3d3d5f2002ec1aa040623c620d467
STUDENT <tranvanhoa15042000@gmail.com>
2020-09-05 18:44:57 +0700

    liên kết bảng

--- backend/functions/sanpham/index.php
+++ backend/functions/sanpham/index.php
@@ -19,109 +19,73 @@
             </div>
             <!-- End sidebar -->
             <!-- Content -->
-            <div class="col-md-10 text-center">
-                <h1>Bảng sản phẩm</h1>
+            <div class="col-md-10">
+                <h1 class="text-center">Bảng sản phẩm</h1>
                 <?php 
                     include_once(__DIR__ . '/../../../dbconnect.php'); 
-                    $sql = " SELECT sp_ma, sp_ten, sp_gia, sp_giacu, sp_mota_ngan, sp_mota_chitiet, sp_ngaycapnhat, sp_soluong, lsp_ma, nsx_ma, km_ma FROM sanpham; ";
+                    $sql = <<<EOT
+                        SELECT sp.*,
+                            lsp.lsp_ten,
+                            nsx.nsx_ten,
+                            km.*
+                        FROM sanpham AS sp
+                        JOIN loaisanpham AS lsp ON sp.lsp_ma = lsp.lsp_ma
+                        JOIN nhasanxuat AS nsx ON sp.nsx_ma = nsx.nsx_ma
+                        LEFT JOIN khuyenmai AS km ON sp.km_ma = km.km_ma;
+EOT;
                     $result = mysqli_query($conn, $sql);
                     $data = [];
                     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                        $km_ten = $row['km_ten'];
+                        $kh_noidung = $row['kh_noidung'];
+                        $kh_tungay = date('d/m/Y',strtotime($row['kh_tungay']));
+                        $km_denngay = date('d/m/Y',strtotime($row['km_denngay']));
+                        $km_tomtat = '';
+                        if(!empty($km_ten)){
+                            $km_tomtat = sprintf("Tên: %s | Nội dung: %s | %s - %s", $km_ten, $kh_noidung, $kh_tungay, $km_denngay);
+                        }
+                        $giacu = '';
+                        if(!empty($row['sp_giacu'])){
+                            $giacu = number_format($row['sp_giacu'],2,'.',',').' vnđ';
+                        }
                         $data[] = array(
                             'sp_ma' => $row['sp_ma'],
                             'sp_ten' => $row['sp_ten'],
-                            'sp_gia' => $row['sp_gia'],
-                            'sp_giacu' => $row['sp_giacu'],
-                            'sp_mota_ngan' => $row['sp_mota_ngan'],
-                            'sp_mota_chitiet' => $row['sp_mota_chitiet'],
-                            'sp_ngaycapnhat' => $row['sp_ngaycapnhat'],
-                            'sp_soluong' => $row['sp_soluong'],
-                            'lsp_ma' => $row['lsp_ma'],
-                            'nsx_ma' => $row['nsx_ma'],
-                            'km_ma' => $row['km_ma'],
+                            'sp_gia' => number_format($row['sp_gia'],2,'.',',').' vnđ',
+                            'sp_giacu' => $giacu,
+                            'lsp_ten' => $row['lsp_ten'],
+                            'nsx_ten' => $row['nsx_ten'],
+                            'km_tomtat' => $km_tomtat,
                         );
                     }
-                    function getlsp($lsp_ma, $conn){
-                        $sql = "SELECT lsp_ten FROM loaisanpham WHERE lsp_ma = $lsp_ma;";
-                        $result = mysqli_query($conn, $sql);
-                        $data = [];
-                        while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-                            $data = array(
-                                'lsp_ten' => $row['lsp_ten']
-                            );
-                        }
-                        return $data['lsp_ten'];
-                    }
-                    function getnsx($nsx_ma, $conn){
-                        $sql = "SELECT nsx_ten FROM nhasanxuat WHERE nsx_ma = $nsx_ma;";
-                        $result = mysqli_query($conn, $sql);
-                        $data = [];
-                        while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-                            $data = array(
-                                'nsx_ten' => $row['nsx_ten']
-                            );
-                        }
-                        return $data['nsx_ten'];
-                    }
-                    function getkm($km_ma, $conn){
-                        $sql = "SELECT km_ten FROM khuyenmai WHERE km_ma = $km_ma;";
-                        $result = mysqli_query($conn, $sql);
-                        $data = [];
-                        while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-                            $data = array(
-                                'km_ten' => $row['km_ten']
-                            );
-                        }
-                        return $data['km_ten'];
-                    }
                 ?>
-                <a href="create.php" class="btn btn-dark m-3">Thêm mới</a>
+                <div class="text-center">
+                    <a href="create.php" class="btn btn-dark m-3">Thêm mới</a>
+                </div>
                 <table class="mx-auto table table-hover">
                     <thead class="thead-dark">
-                        <th>#</th>
                         <th>Mã sản phẩm</th>
                         <th>Tên sản phẩm</th>
                         <th>Giá</th>
                         <th>Giá cũ</th>
-                        <th>Số lượng</th>
                         <th>Loại sản phẩm</th>
                         <th>Nhà sản xuất</th>
                         <th>Khuyến mãi</th>
-                        <th>Ngày cập nhật</th>
                         <th>Action</th>
                     </thead>
                     <tbody>
-                        <?php $i=1 ?>
                         <?php foreach($data as $value):?>
                             <tr>
-                                <td><?= $i++ ?></td>
-                                <td><?=$value['sp_ma']?></td>
-                                <td class="font-weight-bold text-left"><?=$value['sp_ten']?></td>
-                                <td class="text-right"><?=number_format($value['sp_gia'])?></td>
-                                <td class="text-right">
-                                    <?php
-                                        if(!empty($value['sp_giacu'])){
-                                            echo number_format($value['sp_giacu']);
-                                        }
-                                    ?>
-                                </td>
-                                <td><?=$value['sp_soluong']?></td>
+                                <td><?= $value['sp_ma'] ?></td>
+                                <td><?= $value['sp_ten'] ?></td>
+                                <td><?= $value['sp_gia'] ?></td>
+                                <td><?= $value['sp_giacu'] ?></td>
+                                <td><?= $value['lsp_ten'] ?></td>
+                                <td><?= $value['nsx_ten'] ?></td>
+                                <td><?= $value['km_tomtat'] ?></td>
                                 <td>
-                                        <?= getlsp($value['lsp_ma'], $conn);?>
-                                </td>
-                                <td>
-                                        <?= getnsx($value['nsx_ma'], $conn);?>
-                                </td>
-                                <td>
-                                        <?php if(!empty($value['km_ma'])){getkm($value['km_ma'], $conn);}
-                                        
-                                        ?>
-                                </td>
-                                <td><?=$value['sp_ngaycapnhat']?></td>
-                                <td>
-                                    <a href="delete.php?sp_ma=<?=$value['sp_ma']?>">Xóa</a> | 
-                                    <a href="edit.php?sp_ma=<?=$value['sp_ma']?>">Sửa</a> |
-                                    Mô tả
+                                    <a class="btn btn-danger" href="delete.php?sp_ma=<?=$value['sp_ma']?>">Xóa</a> 
+                                    <a class="btn btn-success" href="edit.php?sp_ma=<?=$value['sp_ma']?>">Sửa</a>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
