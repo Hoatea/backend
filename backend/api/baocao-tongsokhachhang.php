77157f743655dccc74e270a0bc44c0a3741f015a
STUDENT <tranvanhoa15042000@gmail.com>
2020-09-15 19:02:42 +0700

    API, AJAX, Chartjs

---
+++ backend/api/baocao-tongsokhachhang.php
@@ -0,0 +1,15 @@
+<?php
+    include_once(__DIR__.'/../../dbconnect.php');
+
+    $sql = "SELECT count(*) AS SoLuong FROM `khachhang`";
+    $result = mysqli_query($conn, $sql);
+    $data = [];
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    {
+        $data[] = array(
+            'SoLuong' => $row['SoLuong'] 
+        );
+    }
+
+    echo json_encode($data[0]);
+?>
---
+++ backend/api/baocao-tongsomathang.php
@@ -0,0 +1,16 @@
+<?php
+    include_once(__DIR__.'/../../dbconnect.php');
+
+    $sql = "SELECT count(*) AS SoLuong FROM `sanpham`";
+    $result = mysqli_query($conn, $sql);
+
+    $data = [];
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    {
+        $data[] = array(
+            'SoLuong' => $row['SoLuong']
+        );
+    }
+
+    echo json_encode($data[0]);
+?>
